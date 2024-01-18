7e1513f9ed5116dacbbefb87bf0245c262b83e28
Steven Gsell <s3068106@students.fscj.edu>
2024-01-18 11:33:53 -0500

    Added the SimpleCalculator class for simple calculating

---
+++ src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculator.java
@@ -0,0 +1,21 @@
+//SimpleCalculator.java
+//Steven Gsell
+//1/18/2024
+//Simple calculator class for math methods
+
+package edu.fscj.cen3024c.simplecalculator;
+
+public class SimpleCalculator {
+    public static int multiply(int number1, int number2) {
+        return number1 * number2;
+    }
+    public static int divide(int number1, int number2) {
+        return number1 / number2;
+    }
+    public static int add(int number1, int number2) {
+        return number1 + number2;
+    }
+    public static int subtract(int number1, int number2) {
+        return number1 - number2;
+    }
+}
--- src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculatorApplication.java
+++ src/main/java/edu/fscj/cen3024c/simplecalculator/SimpleCalculatorApplication.java
@@ -7,6 +7,7 @@ package edu.fscj.cen3024c.simplecalculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static edu.fscj.cen3024c.simplecalculator.SimpleCalculator.*;
 
 @SpringBootApplication
 public class SimpleCalculatorApplication {
